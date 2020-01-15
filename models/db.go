9ff1650cad2359d9b6bb1bf77fec03de9dc20a2c
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2020-01-14 22:32:13 -0500

    Create basic database interface

---
+++ models/db.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Datastore interface {
+}
+
+type DB struct {
+	*mongo.Client
+}
+
+func NewDB(dataSourceName string) (*DB, error) {
+	// Set client options
+	clientOptions := options.Client().ApplyURI(dataSourceName)
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		return nil, err
+	}
+	if err = client.Ping(context.TODO(), nil); err != nil {
+		return nil, err
+	}
+	return &DB{client}, nil
+}
